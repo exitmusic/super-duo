af7417bbfd50f790d0d2ffac536fcc20c36b4961
Kai Chang <kai.chang@morningstar.com>
2015-10-13 19:00:44 -0500

    Create 1x1 widget, initial commit

---
+++ football-scores/app/src/main/java/barqsoft/footballscores/widget/ScoresWidgetProvider.java
@@ -0,0 +1,9 @@
+package barqsoft.footballscores.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by kchang on 10/13/15.
+ */
+public class ScoresWidgetProvider extends AppWidgetProvider {
+}
