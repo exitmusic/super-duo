2628bd1f6e33c0bbe4362027ca422c7a820dd7fb
Kai Chang <kai.chang@morningstar.com>
2015-10-17 17:49:58 -0500

    Change scores widget layout to match scores_list_item initially, populate with static data

---
+++ football-scores/app/src/main/java/barqsoft/footballscores/widget/ScoresWidgetIntentService.java
@@ -0,0 +1,19 @@
+package barqsoft.footballscores.widget;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by kchang on 10/17/15.
+ */
+public class ScoresWidgetIntentService extends IntentService {
+
+    public ScoresWidgetIntentService() {
+        super("ScoresWidgetIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
--- football-scores/app/src/main/java/barqsoft/footballscores/widget/ScoresWidgetProvider.java
+++ football-scores/app/src/main/java/barqsoft/footballscores/widget/ScoresWidgetProvider.java
@@ -28,12 +28,16 @@ public class ScoresWidgetProvider extends AppWidgetProvider {
             RemoteViews views = new RemoteViews(context.getPackageName(), layoutId);
 
             // Add the data to the RemoteViews
-            views.setImageViewResource(R.id.widget_icon, weatherArtResourceId);
+            views.setImageViewResource(R.id.home_crest, weatherArtResourceId);
+            views.setImageViewResource(R.id.away_crest, weatherArtResourceId);
             // Content Descriptions for RemoteViews were only added in ICS MR1
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
                 setRemoteContentDescription(views, description);
             }
-            views.setTextViewText(R.id.widget_high_temperature, formattedMaxTemperature);
+            views.setTextViewText(R.id.home_name, formattedMaxTemperature);
+            views.setTextViewText(R.id.away_name, formattedMaxTemperature);
+            views.setTextViewText(R.id.score_textview, formattedMaxTemperature);
+            views.setTextViewText(R.id.data_textview, formattedMaxTemperature);
 
             // Create an Intent to launch MainActivity
             Intent launchIntent = new Intent(context, MainActivity.class);
@@ -47,6 +51,6 @@ public class ScoresWidgetProvider extends AppWidgetProvider {
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
     private void setRemoteContentDescription(RemoteViews views, String description) {
-        views.setContentDescription(R.id.widget_icon, description);
+        views.setContentDescription(R.id.home_crest, description);
     }
 }
