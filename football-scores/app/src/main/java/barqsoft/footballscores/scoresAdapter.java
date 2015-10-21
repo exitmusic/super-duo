dd1cb10f2ac23d888eb209c2133f7b25da59d2e5
Kai Chang <kai.chang@morningstar.com>
2015-10-20 23:09:43 -0500

    Pass context to Utilities.getLeague() to retrieve translatable strings from strings.xml

--- football-scores/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
+++ football-scores/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
@@ -73,7 +73,7 @@ public class scoresAdapter extends CursorAdapter
                     cursor.getInt(COL_LEAGUE)));
 
             TextView league = (TextView) v.findViewById(R.id.league_textview);
-            league.setText(Utilities.getLeague(cursor.getInt(COL_LEAGUE)));
+            league.setText(Utilities.getLeague(cursor.getInt(COL_LEAGUE), context));
 
             Button share_button = (Button) v.findViewById(R.id.share_button);
             share_button.setContentDescription(context.getString(R.string.a11y_share));
